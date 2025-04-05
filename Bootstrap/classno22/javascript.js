a4ea8144bc8d2f641f7e8debb209d1578b1fb686
Fatimaarshad10 <fatimaarshad091@gmail.com>
2025-04-05 14:33:26 +0500

    Burunu Boma changing done

---
+++ Bootstrap/classno22/javascript.js
@@ -0,0 +1,45 @@
+// name ---> Alphabet 
+// age --> number 
+// weight --> float
+// true 1   , false 0
+
+// Datatype 
+// String --> alphabet , character 
+// integer --> number , 1 ,2,34
+// float --> 1.3 , 3.4
+
+
+// variable 
+// 0010 --> y
+// 0011 --> y 
+
+// name = 'fatima'
+// y
+
+// x = 2
+// y = 10
+// z = x+ y
+
+
+// firstname = 'Noor'
+// lastname = "fatima"
+// age = 10
+// weight = 4.2
+// address = "Saylani house no 02"
+// console.log(firstname)
+// console.log(lastname)
+// console.log(age)
+// console.log(weight)
+// console.log(address)
+
+// console.log('Hello', 23 , 34.2 , 'address')
+
+// firstValue = 10 
+// secondValue = 20 
+// thirdValue = firstValue + secondValue
+// console.log(thirdValue);
+// console.log(thirdValue);
+// console.log(thirdValue)
+// console.log(thirdValue)
+// console.log(thirdValue)
+
